305fab46b74d3a075d7eb49c2a35fbde62a67d72
Ojas Mahajan <ojas.rmahajan23@gmail.com>
2022-01-02 17:20:47 +0530

    added birthday-reminder

---
+++ codedamn-react-projects/birthday-reminder/birthday-reminder/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+	// create a state variable here
+	const [people, setPeople] = useState(data);
+
+	// this should clear all records
+	function clearAllRecords() {
+		setPeople(() => {
+			return [];
+		});
+	}
+	return (
+		<main>
+			<section className="container">
+				{/* change this to state variable data */}
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={clearAllRecords}>Clear All</button>
+			</section>
+		</main>
+	);
+}
+
+export default App;
